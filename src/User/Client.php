1873e8f83bb72591da44b96486110d22e80424f5
xianj2017 <42540403@qq.com>
2020-12-16 00:21:15 +0800

    111

---
+++ src/User/Client.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/3/17
+ * Time: 22:47
+ */
+
+namespace User\Client;
+
+
+class Client
+{
+    /**
+     * No explanation
+     *
+     * @author Zhiqiang Guo
+     * @return void
+     * @throws Exception
+     * @access public
+     */
+    public function index ()
+    {
+        echo "this is client";
+    }
+
+}
